7f382e9b675c6c5767ff5ea953549e9c3fac0fba
CodeSolaris <volkov.aleksandr.it@gmail.com>
2024-08-14 22:59:52 +0300

    feat: implement checkbracket function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const openBrackets = [];
+    const closeBrackets = [];
+    const stack = [];
+
+    for (let arr of bracketsConfig) {
+        openBrackets.push(arr[0]);
+        closeBrackets.push(arr[1]);
+    }
+
+    for (let char of str) {
+        if (openBrackets.includes(char)) {
+            if (
+                char === closeBrackets[openBrackets.indexOf(char)] &&
+                stack[stack.length - 1] === char
+            ) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else {
+            if (!stack.length) {
+                return false;
+            } else {
+                let indexLastElemStack = openBrackets.indexOf(
+                    stack[stack.length - 1]
+                );
+                let indexLastChar = closeBrackets.indexOf(char);
+
+                if (indexLastElemStack === indexLastChar) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
